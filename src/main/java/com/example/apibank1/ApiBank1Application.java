7c0259374123e19b15bb6785677a2a30b74c00f7
Samy-sosa <samysosa069@gmail.com>
2026-08-10 09:39:31 -0600

    chore: initial spring boot project setup whit maven dependencies

---
+++ src/main/java/com/example/apibank1/ApiBank1Application.java
@@ -0,0 +1,13 @@
+package com.example.apibank1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBank1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBank1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apibank1/ApiBank1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apibank1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBank1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
